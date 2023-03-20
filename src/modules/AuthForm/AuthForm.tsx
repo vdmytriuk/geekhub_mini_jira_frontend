5b582474ebc4e6e572097243397032b67c11efcc
Valentin <valaimg2218@gmail.com>
2023-03-20 22:01:53 +0200

    created layout with custom UI for Auth form

--- src/modules/AuthForm/AuthForm.tsx
+++ src/modules/AuthForm/AuthForm.tsx
@@ -1,12 +1,17 @@
 import {FC, useState} from "react";
+import {Link} from "react-router-dom";
 import {useTypedDispatch} from "../../hooks/useTypedDispatch";
 
 import {userActions} from "../../store/user";
 
-import "./AuthForm.scss";
-import {FormField} from "../../UI/FormField/FormField";
+import {ROUTER} from "../../common/config/router";
 import {EMAIL_ERROR_TEXT, EMAIL_REG_EXP, PASS_ERROR_TEXT, PASS_REG_EXP} from "./lib";
+
 import {Button} from "../../UI/Button/Button";
+import {FormField} from "../../UI/FormField/FormField";
+
+import "./AuthForm.scss";
+
 
 const AuthForm: FC = () => {
     const dispatch = useTypedDispatch();
@@ -24,39 +29,53 @@ const AuthForm: FC = () => {
             noValidate
             onSubmit={onSubmit}
         >
-            <h2 className="title">
-                Hello!
-            </h2>
+            <legend className="auth-form__legend">
+                <h2 className="title">
+                    Hello!
+                </h2>
 
-            <p className="text">
-                Ready to pick up where you left off?
-                <br/>
-                Use your email to continue with Kotello.
-            </p>
+                <p className="text">
+                    Ready to pick up where you left off?
+                    <br/>
+                    Use your email to continue with Kotello.
+                </p>
+            </legend>
 
-            <FormField
-                label="Email"
-                type="email"
-                name="email"
-                value={email}
-                setValue={setEmail}
-                pattern={EMAIL_REG_EXP}
-                errorPrompt={EMAIL_ERROR_TEXT}
-            />
+            <div className="auth-form__fields">
+                <FormField
+                    label="Email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    setValue={setEmail}
+                    pattern={EMAIL_REG_EXP}
+                    errorPrompt={EMAIL_ERROR_TEXT}
+                    required
+                />
 
-            <FormField
-                label="Password"
-                type="password"
-                name="password"
-                value={password}
-                setValue={setPassword}
-                pattern={PASS_REG_EXP}
-                errorPrompt={PASS_ERROR_TEXT}
-            />
+                <FormField
+                    label="Password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    setValue={setPassword}
+                    pattern={PASS_REG_EXP}
+                    errorPrompt={PASS_ERROR_TEXT}
+                    required
+                />
+            </div>
+
+            <Link className="text" to={ROUTER.FORGOT_PASSWORD}>
+                Forgot password?
+            </Link>
 
             <Button type="submit">
                 Sign in
             </Button>
+
+            <p className="auth-form__bottom text">
+                Don’t have an account? <Link to={ROUTER.REGISTRATION}>Sign up here</Link>
+            </p>
         </form>
     );
 };
--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,12 +1,13 @@
 import {FC} from "react";
 
 import AuthForm from "../../modules/AuthForm/AuthForm";
+import {PublicLayout} from "../../layouts/PublicLayout";
 
 const AuthPage:FC = () => {
     return (
-        <>
+        <PublicLayout>
             <AuthForm/>
-        </>
+        </PublicLayout>
     );
 };
 
