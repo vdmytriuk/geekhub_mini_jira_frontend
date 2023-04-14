7022c6620ab083fa1819623c247ac8597603035d
Valentin <valaimg2218@gmail.com>
2023-04-15 00:18:50 +0300

    updated task

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,12 @@ import {combineReducers, configureStore} from '@reduxjs/toolkit';
 
 import {userReducer} from "./user";
 import {projectsReducer} from "../modules/Dashboard";
+import {taskReducer} from "../modules/Task";
 
 const rootReducer = combineReducers({
     user: userReducer,
-    projects: projectsReducer
+    projects: projectsReducer,
+    task: taskReducer,
 });
 
 export const store = configureStore({
